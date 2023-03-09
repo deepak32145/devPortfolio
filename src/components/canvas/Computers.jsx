4b9e7eb1414b6d68fe3887e58f987a43319c8962
deepak choudhary <deepak32145@gmail.com>
2023-03-09 11:06:45 +0530

    error in mobile device

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,12 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import Loader from "../Loader";
 
-const Computers = (isMobile) => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
@@ -29,17 +31,28 @@ const Computers = (isMobile) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width  : 500px)");
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
+
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -56,6 +69,7 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
